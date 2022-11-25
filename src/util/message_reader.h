781583631beeb602d46522a097b0a4e005659e5b
black-desk <me@black-desk.cn>
2022-11-25 14:18:05 +0800

    chore: Update SPDX comment

--- src/util/message_reader.h
+++ src/util/message_reader.h
@@ -1,11 +1,7 @@
 /*
- * Copyright (c) 2020-2021. Uniontech Software Ltd. All rights reserved.
+ * SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
  *
- * Author:     Linxuan Chen <chenlinxuan@uniontech.com>
- *
- * Maintainer: Linxuan Chen <chenlinxuan@uniontech.com>
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
 #ifndef LINGLONG_BOX_SRC_UTIL_MESSAGE_READER_H_
