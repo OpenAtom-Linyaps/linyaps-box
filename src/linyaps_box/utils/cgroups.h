2e9ddbfb9fbb78a364966942d075b8a5f65185bc
ComixHe <heyuming@deepin.org>
2025-03-07 18:31:34 +0800

    refactor: adjust remounting process
     - use a workwound for /sys/fs/cgroup
     - adjust the sequence of mounting process
     - adjust mount propagation process
    
    Signed-off-by: ComixHe <heyuming@deepin.org>

--- src/linyaps_box/utils/cgroups.h
+++ src/linyaps_box/utils/cgroups.h
@@ -5,10 +5,11 @@
 #pragma once
 
 #include <cstdint>
+
 namespace linyaps_box::utils {
 
 enum class cgroup_t : std::uint16_t { unified, legacy, hybrid };
 
 cgroup_t get_cgroup_type();
 
-}
+} // namespace linyaps_box::utils
--- src/linyaps_box/utils/fstat.h
+++ src/linyaps_box/utils/fstat.h
@@ -6,6 +6,8 @@
 
 #include "linyaps_box/utils/file_describer.h"
 
+#include <sys/statfs.h>
+
 #include <fcntl.h>
 #include <sys/stat.h>
 
@@ -28,6 +30,17 @@ inline struct stat lstat(const file_descriptor &fd)
     auto ret = ::fstatat(fd.get(), "", &statbuf, AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW);
     if (ret == -1) {
         throw std::system_error(errno, std::generic_category(), "fstatat");
+    }
+
+    return statbuf;
+}
+
+inline struct statfs statfs(const file_descriptor &fd)
+{
+    struct statfs statbuf{};
+    auto ret = ::statfs(fd.proc_path().c_str(), &statbuf);
+    if (ret == -1) {
+        throw std::system_error(errno, std::generic_category(), "statfs");
     }
 
     return statbuf;
