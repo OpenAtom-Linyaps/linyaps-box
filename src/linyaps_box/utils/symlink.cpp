c51aa250de036bc5494ac8dcd20797ce35a2403b
ComixHe <ComixHe1895@outlook.com>
2025-07-01 20:06:43 +0800

    feat: support runtime Dev symbolic link
    Signed-off-by: ComixHe <ComixHe1895@outlook.com>

---
+++ src/linyaps_box/utils/symlink.cpp
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include "linyaps_box/utils/symlink.h"
+
+#include "linyaps_box/utils/log.h"
+
+void linyaps_box::utils::symlink(const std::filesystem::path &target,
+                                 const std::filesystem::path &link_path)
+{
+    LINYAPS_BOX_DEBUG() << "Create symlink " << link_path << " point to " << target;
+
+    std::error_code ec;
+    std::filesystem::create_symlink(target, link_path, ec);
+    if (ec) {
+        throw std::system_error{ ec.value(), std::system_category(), ec.message() };
+    }
+}
+
+void linyaps_box::utils::symlink_at(const std::filesystem::path &target,
+                                    const file_descriptor &dirfd,
+                                    const std::filesystem::path &link_path)
+{
+    LINYAPS_BOX_DEBUG() << "Create symlink " << link_path << " which under " << dirfd.current_path()
+                        << " point to " << target;
+
+    auto ret = ::symlinkat(target.c_str(), dirfd.get(), link_path.c_str());
+    if (ret == -1) {
+        throw std::system_error(errno, std::system_category(), "symlinkat");
+    }
+}
