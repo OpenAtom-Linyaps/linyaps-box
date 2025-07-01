35ac00a66ad52640676e3b3cdbba6b810e58bcf8
ComixHe <ComixHe1895@outlook.com>
2025-07-01 20:06:43 +0800

    feat: support runtime Dev symbolic link
    Signed-off-by: ComixHe <ComixHe1895@outlook.com>

---
+++ src/linyaps_box/utils/symlink.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include "linyaps_box/utils/file_describer.h"
+
+#include <filesystem>
+
+namespace linyaps_box::utils {
+
+void symlink(const std::filesystem::path &source, const std::filesystem::path &target);
+
+void symlink_at(const std::filesystem::path &target,
+               const file_descriptor &dirfd,
+               const std::filesystem::path &link_path);
+
+} // namespace linyaps_box::utils
