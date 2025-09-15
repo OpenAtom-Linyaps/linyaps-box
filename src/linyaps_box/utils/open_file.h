866bffae620a717a1dfddb254218122ba71782cf
ComixHe <ComixHe1895@outlook.com>
2025-09-15 14:29:09 +0800

    refactor: change the default open flag
    make O_PATH as the default flag.
    
    Signed-off-by: ComixHe <ComixHe1895@outlook.com>

--- src/linyaps_box/utils/open_file.h
+++ src/linyaps_box/utils/open_file.h
@@ -12,12 +12,12 @@
 
 namespace linyaps_box::utils {
 
-auto open(const std::filesystem::path &path, int flag = O_RDONLY, mode_t mode = 0)
+auto open(const std::filesystem::path &path, int flag = O_PATH | O_CLOEXEC, mode_t mode = 0)
         -> file_descriptor;
 
 auto open_at(const file_descriptor &root,
              const std::filesystem::path &path,
-             int flag = O_RDONLY,
+             int flag = O_PATH | O_CLOEXEC,
              mode_t mode = 0) -> file_descriptor;
 
 } // namespace linyaps_box::utils
