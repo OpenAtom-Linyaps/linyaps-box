20f0a668e2fbd80038948139274bee8b24249594
Yuming He <ComixHe1895@outlook.com>
2026-08-12 17:40:04 +0800

    fix: use non-locale fmt api for compability
    Signed-off-by: Yuming He <ComixHe1895@outlook.com>

--- src/linyaps_box/log/logger.cpp
+++ src/linyaps_box/log/logger.cpp
@@ -159,7 +159,8 @@ void global_logger::dispatch_log(level lvl,
 
     thread_local fmt::memory_buffer buf;
     buf.clear();
-    fmt::vformat_to(std::back_inserter(buf), fmt::locale_ref{ }, fmt_str, args);
+
+    fmt::vformat_to(std::back_inserter(buf), fmt_str, args);
 
     const log_context ctx{
         lvl,
