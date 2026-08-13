fc456d566372719adf9949b0b8b4aab93ea5d8b0
Yuming He <ComixHe1895@outlook.com>
2026-08-13 15:59:35 +0800

    feat(rootfs): emulate fs.protected_symlinks in userspace resolution
    Adjust source structure and remove some uneeded files
    
    Signed-off-by: Yuming He <ComixHe1895@outlook.com>

---
+++ src/linyaps_box/os/io.cpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2026 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include "linyaps_box/os/io.h"
+
+#include "linyaps_box/utils/utils.h"
+
+namespace linyaps_box::os {
+auto read(utils::file_descriptor_ref fd, utils::span<std::byte> buf) noexcept -> Result<std::size_t>
+{
+    while (true) {
+        auto ret = ::read(fd, buf.data(), buf.size());
+        if (LIKELY(ret >= 0)) {
+            return ret;
+        }
+
+        if (errno == EINTR) {
+            continue;
+        }
+
+        return unexpected{ make_error_code(errno) };
+    }
+}
+} // namespace linyaps_box::os
