2f5329e586a4f9153715b23231ce875f07eb9eee
Yuming He <ComixHe1895@outlook.com>
2026-08-26 15:47:08 +0800

    build: unify dependency resolution behind OFFLINE_BUILD
    Signed-off-by: Yuming He <ComixHe1895@outlook.com>

---
+++ cmake.external/fmt/Findfmt.cmake
@@ -0,0 +1,9 @@
+if(TARGET fmt::fmt)
+  set(fmt_FOUND TRUE)
+
+  # Set version variables
+  set(fmt_VERSION_MAJOR 12)
+  set(fmt_VERSION_MINOR 2)
+  set(fmt_VERSION_PATCH 0)
+  set(fmt_VERSION "12.2.0")
+endif()
